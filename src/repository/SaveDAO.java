acaa08c9de7f6798dd8d9eecb84b1f78eeb292f7
alanakov <angelikovalski@gmail.com>
2024-09-12 18:31:42 -0300

    adicionando o comando save e load, funcionando corretamente mas precisa alterar a descrição quando o usuario da get, precisa exibir uma mensagem de erro caso o usuario digitar por exemplo, save 1, mas ja possui o save 1, entao da erro

--- src/repository/SaveDAO.java
+++ src/repository/SaveDAO.java
@@ -1,31 +1,54 @@
 package repository;
 
-import config.MySql;
-import model.Save;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SaveDAO {
-    public static Save findSaveById(Integer id) throws SQLException {
-        Connection conn = MySql.getConnection();
-        String sql = "SELECT * FROM save WHERE idSave = ?";
 
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setInt(1, id);
+    // Salva o progresso do jogo
+    public static void salvarProgresso(int cenaAtualId, int saveId) throws SQLException {
+        String query = "INSERT INTO save (idSave, idSaveCenaAtual) VALUES (?, ?)";
 
-        ResultSet rs = ps.executeQuery();
-        Save save = null;
-
-        if (rs.next()) {
-            save = new Save(
-                    rs.getInt("idSave"),
-                    rs.getInt("idSaveCenaAtual"),
-                    rs.getString("data")
-            );
+        try (Connection conn = repository.DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, saveId);
+            pstmt.setInt(2, cenaAtualId);
+            pstmt.executeUpdate();
         }
-        return save;
+    }
+
+    // Carrega o progresso com base no ID do save
+    public static int carregarProgresso(int saveId) throws SQLException {
+        String query = "SELECT idSaveCenaAtual FROM save WHERE idSave = ?";
+
+        try (Connection conn = repository.DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            pstmt.setInt(1, saveId);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("idSaveCenaAtual");
+            } else {
+                return -1; // Indica que o salvamento não foi encontrado
+            }
+        }
+    }
+
+    // Lista todos os IDs de salvamento
+    public static List<Integer> listarSaves() throws SQLException {
+        String query = "SELECT idSave FROM save";
+        List<Integer> saves = new ArrayList<>();
+
+        try (Connection conn = repository.DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                saves.add(rs.getInt("idSave"));
+            }
+        }
+        return saves;
     }
 }
