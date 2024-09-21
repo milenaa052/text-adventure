129332962d9f39b8b54c43c9c9ef7bc044856e0f
milenaa052 <milenasantosdeoliveira40@gmail.com>
2024-09-21 10:53:43 -0300

    Comando start e algumas correções

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
+            Scanner scanner = new Scanner(System.in);
+
+            // Exibe a mensagem inicial e espera pelo comando START
+            System.out.println("Digite START para iniciar o jogo");
+            String comandoInicial;
+            do {
+                comandoInicial = scanner.nextLine().trim();
+                if (!comandoInicial.equalsIgnoreCase("START")) {
+                    System.out.println("Comando inválido. Digite START para iniciar o jogo.");
+                }
+            } while (!comandoInicial.equalsIgnoreCase("START")); // Só prossegue quando o comando for "START"
+
             int cenaInicialId = 1; //define o id da cena inicial como 1
             GameController gameController = new GameController(cenaInicialId); //cria instância do game controller passando o id 1
-            Scanner scanner = new Scanner(System.in);
 
             Cena cenaAtual = CenaDAO.findCenaById(gameController.getCenaAtualId()); // pega a cena atual usando o id da cena do gameController
             if (cenaAtual != null) { //se a cena não for nula exibe a descrição da cena atual
